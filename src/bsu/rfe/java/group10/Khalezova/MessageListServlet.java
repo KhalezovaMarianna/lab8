e6250cc409dff3f415e7c2c9e3a55c21b82ceb49
KhalezovaMarianna <rct.halezova@bsu.by>
2021-05-19 12:40:25 +0300

    project

---
+++ src/bsu/rfe/java/group10/Khalezova/MessageListServlet.java
@@ -0,0 +1,29 @@
+package bsu.rfe.java.group10.Khalezova;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import bsu.rfe.java.group8.lab8.Yury.varA4.entity.ChatMessage;
+
+public class MessageListServlet extends ChatServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse
+            response) throws ServletException, IOException {
+        // Установить кодировку HTTP-ответа UTF-8
+        response.setCharacterEncoding("utf8");
+        // Получить доступ к потоку вывода HTTP-ответа
+        PrintWriter pw = response.getWriter();
+        // Записть в поток HTML-разметку страницы
+        pw.println("<html><head><meta http-equiv='Content-Type' " +
+                "content='text/html; charset=utf-8'/><meta http-equiv='refresh' " +
+                "content='10'></head>");
+        pw.println("<body>");
+
+    }
+}
